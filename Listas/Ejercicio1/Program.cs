1f14344d5916cd6656730b889ba16508a8652fd2
Nagore <reina.nagore@gmail.com>
2019-10-14 16:59:44 +0200

    Ejercicios 1 y 2 terminados, ejercicio 3 creado

---
+++ Listas/Ejercicio1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fruta = new List<string>();
+            CrearLista(fruta);
+            MostrarLista(fruta);
+            Console.WriteLine("\n");
+            OrdenarLista(fruta);
+        }
+        public static void CrearLista(List <String> f)
+        {
+            f.Add("Manzana");
+            f.Add("Naranja");
+            f.Add("Platano");
+            f.Add("Pera");
+            f.Add("Limon");
+        }
+        public static void MostrarLista(List<String> f)
+        {
+            foreach (String x in f)
+            {
+                Console.WriteLine(x);
+            }
+        }
+        public static void OrdenarLista(List<String> f)
+        {
+            f.Sort();
+            MostrarLista(f);
+        }
+        public static void BorrarDeLaLista(List<String> f, string x)
+        {
+            f.Remove(x);
+        }
+        public static int ContarElementos(List<string> f)
+        {
+            return f.Count;
+        }
+    }
+}
---
+++ Listas/Ejercicio2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> enteros = new List<int>();
+            List<double> decimales = new List<double>();
+            double num = 0;
+            do
+            {
+                Console.WriteLine("Introduce un valor");
+                string textoUsuario = Console.ReadLine();
+                if (double.TryParse(textoUsuario, out num))
+                {
+                    if (num >= 0)
+                    {
+                        if (textoUsuario.Contains("."))
+                        {
+                            decimales.Add(num);
+                        }
+                        else
+                        {
+                            enteros.Add(Convert.ToInt32(num));
+                        }
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERROR: NO HAS INTRODUCIDO UN NUMERO");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+            } while (num >= 0);
+            Console.WriteLine("Enteros:");
+            int sumaEnteros = 0;
+            double sumaDecimales = 0;
+            foreach (int x in enteros)
+            {
+                Console.Write($"{x}, ");
+                sumaEnteros += x;
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Decimales:");
+            foreach (double x in decimales)
+            {
+                Console.Write($"{x}, ");
+                sumaDecimales += x;
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"La suma de todos los enteros es: {sumaEnteros}");
+            Console.WriteLine($"La media de todos los numeros es: {(sumaEnteros+sumaDecimales)/(enteros.Count+decimales.Count)}");
+
+
+        }
+    }
+}
